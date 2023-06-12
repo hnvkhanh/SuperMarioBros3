5c4c1cc5598781591684853c0b48b1c1f81b26ae
hnvkhanh <hnvk101@gmail.com>
2023-06-12 14:31:36 +0700

    make mushroom moving

--- Prize.cpp
+++ Prize.cpp
@@ -2,9 +2,13 @@
 
 void CMushroom::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 {
-	if (y > y_rise) {
-		y += vy * dt;
+	if (y <= y_rise) {
+		/*y += vy * dt;*/
+		SetState(MUSHROOM_STATE_MOVING);
 	}
+	vy += ay * dt;
+	/*y += vy * dt;
+	x += vx * dt;*/
 	
 	CGameObject::Update(dt, coObjects);
 	CCollision::GetInstance()->Process(this, dt, coObjects);
@@ -18,6 +22,22 @@ void CMushroom::Render()
 	//RenderBoundingBox();
 }
 
+void CMushroom::SetState(int state)
+{
+	CGameObject::SetState(state);
+	switch (state)
+	{
+	case  MUSHROOM_STATE_RISING:
+		vy = -MUSHROOM_RISING_SPEED;
+		break;
+	case  MUSHROOM_STATE_MOVING:
+		vy = 0;
+		ay = MUSHROOM_GRAVITY;
+		vx = MUSHROOM_SPEED;
+		break;	
+	}
+}
+
 void CMushroom::GetBoundingBox(float& l, float& t, float& r, float& b)
 {
 	l = x - MUSHROOM_BBOX_WIDTH / 2;
@@ -25,3 +45,24 @@ void CMushroom::GetBoundingBox(float& l, float& t, float& r, float& b)
 	r = l + MUSHROOM_BBOX_WIDTH;
 	b = t + MUSHROOM_BBOX_HEIGHT;
 }
+
+void CMushroom::OnCollisionWith(LPCOLLISIONEVENT e)
+{
+	if (!e->obj->IsBlocking()) return;	
+	
+
+	if (e->ny != 0)
+	{		
+		vy = 0;		
+	}
+	else if (e->nx != 0)
+	{
+		vx = -vx;
+	}
+}
+
+void CMushroom::OnNoCollision(DWORD dt)
+{
+	x += vx * dt;
+	y += vy * dt;
+};
--- Prize.h
+++ Prize.h
@@ -5,7 +5,9 @@
 
 #include "Brick.h"
 
-#define MUSHROOM_SPEED 0.01f
+#define MUSHROOM_SPEED 0.07f
+#define MUSHROOM_RISING_SPEED 0.01f
+#define MUSHROOM_GRAVITY 0.002f
 
 #define ID_ANI_MUSHROOM 11100
 
@@ -21,15 +23,22 @@
 class CMushroom : public CGameObject
 {
 protected:
-	float y_rise;
+	float y_rise, ay;
+	int state;
+
+	virtual void OnNoCollision(DWORD dt);
+	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
+	virtual int IsCollidable() { return 1; };
 public:
 	CMushroom(float x, float y) : CGameObject(x, y) {
-		this->y_rise = y - BRICK_BBOX_HEIGHT/2;
-		this->vy = -MUSHROOM_SPEED;
+		this->y_rise = y - BRICK_BBOX_HEIGHT/2;	
+		this->ay = 0;
+		SetState(MUSHROOM_STATE_RISING);
 	};
 	void GetBoundingBox(float& l, float& t, float& r, float& b);
 	void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	void Render();	
 	int IsBlocking() { return 0; }
+	virtual void SetState(int state);
 };
 
